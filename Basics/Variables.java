ad30d7bb4b1dd6657aa267348d13e387eb87e7dd
RICHA PANDEY <richapandey03.vaar@gmail.com>
2026-07-18 23:27:03 +0530

    Initial Commit

---
+++ Basics/Variables.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 10;
+        String myString = "Hello, Java!";
+        boolean myBoolean = true;
+
+        System.out.println("Integer: " + myNumber);
+        System.out.println("String: " + myString);
+        System.out.println("Boolean: " + myBoolean);
+    }
+}
