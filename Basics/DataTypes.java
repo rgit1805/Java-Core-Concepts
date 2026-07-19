e4233c34b495fae08c58b7bd04817a60728a46a9
RICHA PANDEY <richapandey03.vaar@gmail.com>
2026-07-19 22:59:35 +0530

    Commit

---
+++ Basics/DataTypes.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class DataTypes{
+    public static void main(String[] args){
+        int myInt = 42;
+        double myDouble = 3.14;
+        char myChar = 'A';
+        boolean myBool = false;
+
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Character: " + myChar);
+        System.out.println("Boolean: " + myBool);
+    }
+}
